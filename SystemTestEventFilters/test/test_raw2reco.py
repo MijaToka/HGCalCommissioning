877e6a9e25570a29b575faed7bffa7fa0d50e0d7
IzaakWN <iwn_@hotmail.com>
2024-04-16 15:10:46 +0200

    remove deprecated HGCalEmulatedSlinkRawData

--- SystemTestEventFilters/test/test_raw2reco.py
+++ SystemTestEventFilters/test/test_raw2reco.py
@@ -1,3 +1,5 @@
+# Instructions:
+#   cmsRun -j FrameworkJobReport_Run1695762407_Link2_File0000000001_RECO.xml $CMSSW_BASE/src/HGCalCommissioning/SystemTestEventFilters/test/test_raw2reco.py mode=slinkfromraw slinkBOE=0x55 cbHeaderMarker=0x7f econdHeaderMarker=0x154 bePassTrough=False inputFiles=/eos/cms/store/group/dpg_hgcal/tb_hgcal/2023/BeamTestSep/HgcalBeamtestSep2023/Relay1695762407/Run1695762407_Link1_File0000000001.bin,/eos/cms/store/group/dpg_hgcal/tb_hgcal/2023/BeamTestSep/HgcalBeamtestSep2023/Relay1695762407/Run1695762407_Link2_File0000000001.bin fedId=1,2 inputTrigFiles=/eos/cms/store/group/dpg_hgcal/tb_hgcal/2023/BeamTestSep/HgcalBeamtestSep2023/Relay1695762407/Run1695762407_Link0_File0000000001.bin output=Run1695762407_Link2_File0000000001 runNumber=1695762407 maxEvents=1000000000
 # Based on
 #   https://github.com/CMS-HGCAL/cmssw/blob/hgcal-condformat-HGCalNANO-13_2_0_pre3_linearity/EventFilter/HGCalRawToDigi/test/tb_raw2reco.py
 #   https://gitlab.cern.ch/hgcal-dpg/hgcal-comm/-/blob/master/SystemTestEventFilters/test/test_slink_source.py
@@ -32,36 +34,14 @@ options.register('sipmcells','Geometry/HGCalMapping/data/CellMaps/channels_sipmo
 # RAW -> DIGI options:
 options.register('mode', 'trivial', VarParsing.multiplicity.singleton, VarParsing.varType.string,
                  "type of emulation")
-#options.register('numCaptureBlocks', 1, VarParsing.multiplicity.singleton, VarParsing.varType.int,
-#                 "number of capture blocks to emulate")
-#options.register('numECONDs', 1, VarParsing.multiplicity.singleton, VarParsing.varType.int,
-#                 "number of ECON-Ds to emulate")
-#options.register('numChannelsPerERx', 37, VarParsing.multiplicity.singleton, VarParsing.varType.int,
-#                 "number of channels enabled per ERx")
-#options.register('numERxsPerECOND', 6, VarParsing.multiplicity.singleton, VarParsing.varType.int,
-#                 "number of ERxs enabled per ECON-D")
-#options.register('activeECONDs', [0], VarParsing.multiplicity.list, VarParsing.varType.int,
-#                 "list of ECON-Ds enabled")
-#options.register('ECONDsInPassthrough', [0], VarParsing.multiplicity.list, VarParsing.varType.int,
-#                 "list of ECON-Ds in passthrough mode")
-#options.register('ECONDsInCharacterisation', [], VarParsing.multiplicity.list, VarParsing.varType.int,
-#                 "list of ECON-Ds in characterisation mode")
-#options.register('ECONDToTStatus', 3, VarParsing.multiplicity.singleton, VarParsing.varType.int,
-#                 "default ToT status bits (aka TcTp bits) value to be emulated")
-#options.register('randomActiveCaptureBlocks', False, VarParsing.multiplicity.singleton, VarParsing.varType.int,
-#                 "randomly activate capture blocks on emulation")
-#options.register('randomActiveECOND', False, VarParsing.multiplicity.singleton, VarParsing.varType.int,
-#                 "randomly activate ECOn-Ds on emulation")
-#options.register('slinkBOE', 0x2a, VarParsing.multiplicity.singleton, VarParsing.varType.int,
-#                 "Begin of event marker for S-link")
-#options.register('cbHeaderMarker', 0x5f, VarParsing.multiplicity.singleton, VarParsing.varType.int,
-#                 "Begin of event marker for BE/capture block")
-#options.register('econdHeaderMarker', 0x154, VarParsing.multiplicity.singleton, VarParsing.varType.int,
-#                 "Begin of event marker for ECON-D")
-#options.register('applyFWworkaround', False, VarParsing.multiplicity.singleton, VarParsing.varType.bool,
-#                 "Patch unpacker behavior to deal with firmware known features")
-#options.register('swap32bendianness', False, VarParsing.multiplicity.singleton, VarParsing.varType.bool,
-#                 "Swap 32b endianness in the raw data")
+options.register('slinkBOE', 0x2a, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                 "Begin of event marker for S-link")
+options.register('cbHeaderMarker', 0x5f, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                 "Begin of event marker for BE/capture block")
+options.register('econdHeaderMarker', 0x154, VarParsing.multiplicity.singleton, VarParsing.varType.int,
+                 "Begin of event marker for ECON-D")
+options.register('bePassTrough', False, VarParsing.multiplicity.singleton, VarParsing.varType.bool,
+                 "Ignore ECON-D packet mismatches") # patch unpacker behavior to deal with firmware known features
 # module calibration & configurations:
 options.register('params',"",mytype=VarParsing.varType.string,
                  info="Path to calibration parameters (JSON format)")
@@ -78,6 +58,8 @@ if not options.params:
   #options.params = f"{outdir}/calibration_parameters_v2.json"
   options.params = f"{outdir}/level0_calib_params.json"
 if not options.modules:
+  # git clone https://github.com/pfs/Geometry-HGCalMapping.git $CMSSW_BASE/src/Geometry/HGCalMapping/data
+  # TODO: move to https://gitlab.cern.ch/hgcal-dpg/hgcal-comm
   #options.modules = "Geometry/HGCalMapping/data/ModuleMaps/modulelocator_test.txt" # test beam
   options.modules = "Geometry/HGCalMapping/data/ModuleMaps/modulelocator_test_2mods.txt" # only first two modules
 print(f">>> fedIds:       {options.fedId!r}")
@@ -149,75 +131,18 @@ process.hgcalCalibESProducer = cms.ESProducer( # ESProducer to load calibration 
   moduleIndexerSource=cms.ESInputTag('')
 )
 
-# RAW UNPACKER
-#print(">>> Prepare raw unpacker...")
-# HGCalCommissioning/SystemTestEventFilters/plugins/HGCalSlinkEmulator.cc
-process.load('HGCalCommissioning.SystemTestEventFilters.hgcalEmulatedSlinkRawData_cfi')
-process.load('Configuration.StandardSequences.Accelerators_cff')
-process.load('HeterogeneousCore.AlpakaCore.ProcessAcceleratorAlpaka_cfi')
-process.hgcalEmulatedSlinkRawData.emulatorType = options.mode
-process.hgcalEmulatedSlinkRawData.fedId = cms.uint32(options.fedId[0][0])
-###if process.hgcalEmulatedSlinkRawData.emulatorType=='hgcmodule':
-###  #process.hgcalEmulatedSlinkRawData.treeName = cms.untracked.string('unpacker_data/hgcroc')
-###  process.hgcalEmulatedSlinkRawData.inputs = cms.untracked.vstring(*options.inputFiles)
-###  process.hgcalEmulatedSlinkRawData.storeEmulatorInfo = bool(options.storeEmulatorInfo)
-###elif process.hgcalEmulatedSlinkRawData.emulatorType=='trivial': #'slinkfromraw':
-###  process.hgcalEmulatedSlinkRawData.fedIds = cms.untracked.vuint32(*options.fedId)
-###  process.hgcalEmulatedSlinkRawData.inputs = cms.untracked.vstring(*options.inputFiles)
-###  process.hgcalEmulatedSlinkRawData.trig_inputs = cms.untracked.vstring(*options.inputTrigFiles)
-process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
-  hgcalEmulatedSlinkRawData=cms.PSet(initialSeed=cms.untracked.uint32(42)),
-  #engineName = cms.untracked.string('TRandom3')
-)
-
-## RAW UNPACKER: steer the number of capture blocks
-#if options.randomActiveCaptureBlocks:
-#  from random import randint
-#  # randomise the number of capture blocks emulated
-#  process.hgcalEmulatedSlinkRawData.slinkParams.numCaptureBlocks = randint(1, 50)
-#else:
-#  process.hgcalEmulatedSlinkRawData.slinkParams.numCaptureBlocks = options.numCaptureBlocks
-#print(f"S-link: number of capture blocks: {process.hgcalEmulatedSlinkRawData.slinkParams.numCaptureBlocks.value()}")
-#
-## RAW UNPACKER: steer the number (and/or list) of ECON-Ds per capture block
-#if options.numECONDs > 0:
-#    for i in range(options.numECONDs - len(process.hgcalEmulatedSlinkRawData.slinkParams.ECONDs)):
-#        process.hgcalEmulatedSlinkRawData.slinkParams.ECONDs.append(
-#            process.hgcalEmulatedSlinkRawData.slinkParams.ECONDs[0].clone())
-#    process.hgcalEmulatedSlinkRawData.slinkParams.checkECONDsLimits = False  # allows to mess with unconventional, high number
-#    # of ECON-Ds per capture block
-#econd_id = 0
-#for econd in process.hgcalEmulatedSlinkRawData.slinkParams.ECONDs:
-#  # must use 'cms.' python configuration types
-#  if options.randomActiveECOND:  # randomly turn on/off any ECON-D in capture block
-#    from random import getrandbits
-#    econd.active = cms.bool(bool(getrandbits(1)))
-#  else:  # use a fixed, user-steered list of ECON-Ds in capture block
-#    econd.active = cms.bool((econd_id in options.activeECONDs))
-#  econd.passthroughMode = cms.bool((econd_id in options.ECONDsInPassthrough))
-#  econd.characterisationMode = cms.bool((econd_id in options.ECONDsInCharacterisation))
-#  econd.enabledERxs = cms.vuint32([i for i in range(options.numERxsPerECOND)])
-#  econd.numChannelsPerERx = cms.uint32(options.numChannelsPerERx)
-#  econd.defaultToTStatus = cms.uint32(options.ECONDToTStatus)
-
 # RAW -> DIGI producer
 #print(">>> Prepare RAW -> DIGI...")
 process.load('EventFilter.HGCalRawToDigi.hgcalDigis_cfi')
-#process.hgcalDigis.src = cms.InputTag('hgcalEmulatedSlinkRawData', 'hgcalFEDRawData')
-process.hgcalDigis.src = cms.InputTag('source')
+process.hgcalDigis.src = cms.InputTag('HGCalSlinkFromRawSource', 'hgcalFEDRawData')
+#process.hgcalDigis.src = cms.InputTag('source')
 process.hgcalDigis.fedIds = cms.vuint32(*options.fedId)
-process.hgcalDigis.maxCaptureBlock = process.hgcalEmulatedSlinkRawData.slinkParams.numCaptureBlocks
-#process.hgcalDigis.numERxsInECOND = options.numERxsPerECOND
-#process.hgcalDigis.captureBlockECONDMax = max(  # allows to mess with unconventional, high number of ECON-Ds per capture block
-#    process.hgcalDigis.captureBlockECONDMax,
-#    len([ec for ec in process.hgcalEmulatedSlinkRawData.slinkParams.ECONDs if ec.active]))
-#process.hgcalDigis.configSource = cms.ESInputTag('')  # for HGCalConfigESSourceFromYAML
-#process.hgcalDigis.moduleInfoSource = cms.ESInputTag('')  # for HGCalModuleInfoESSource
-#process.hgcalDigis.slinkBOE = cms.uint32(options.slinkBOE)
-#process.hgcalDigis.cbHeaderMarker = cms.uint32(options.cbHeaderMarker)
-#process.hgcalDigis.econdHeaderMarker = cms.uint32(options.econdHeaderMarker)
-#process.hgcalDigis.applyFWworkaround = options.applyFWworkaround
-#process.hgcalDigis.swap32bendianness = options.swap32bendianness
+process.hgcalDigis.configSource = cms.ESInputTag('')  # for HGCalConfigESSourceFromYAML
+process.hgcalDigis.moduleInfoSource = cms.ESInputTag('')  # for HGCalModuleInfoESSource
+process.hgcalDigis.slinkBOE = cms.uint32(options.slinkBOE) # TODO: Izaak move to config
+#process.hgcalDigis.cbHeaderMarker = cms.uint32(options.cbHeaderMarker) # TODO: ask Huilin, Izaak move to config
+process.hgcalDigis.econdHeaderMarker = cms.uint32(options.econdHeaderMarker) # TODO: Izaak move to config
+process.hgcalDigis.bePassTrough = options.bePassTrough # TODO: Izaak move to config
 
 ## FILTER empty events
 #process.load('EventFilter.HGCalRawToDigi.hgCalEmptyEventFilter_cfi')
@@ -226,6 +151,8 @@ process.hgcalDigis.maxCaptureBlock = process.hgcalEmulatedSlinkRawData.slinkPara
 
 # DIGI -> RECO producer
 #print(">>> Prepare DIGI -> RECO...")
+process.load('Configuration.StandardSequences.Accelerators_cff')
+process.load('HeterogeneousCore.AlpakaCore.ProcessAcceleratorAlpaka_cfi')
 process.load('HeterogeneousCore.CUDACore.ProcessAcceleratorCUDA_cfi')
 if options.gpu:
   process.hgcalRecHit = cms.EDProducer(
@@ -251,10 +178,9 @@ else:
 # MAIN PROCESSES
 #print(">>> Prepare process...")
 process.p = cms.Path(
-  process.hgcalEmulatedSlinkRawData     # RAW GENERATION
   #*process.hgCalEmptyEventFilter       # FILTER empty events
-  *process.hgcalDigis                   # RAW -> DIGI
-  #*process.hgcalRecHit                 # DIGI -> RECO (RecHit)
+  process.hgcalDigis                    # RAW -> DIGI
+  #*process.hgcalRecHit                 # DIGI -> RECO (RecHit calibrations)
   #*process.hgCalRecHitsFromSoAproducer # RECO -> NANO Phase I format translator
 )
 
@@ -263,7 +189,8 @@ if options.dumpFRD:
   #print(">>> Adding dumpFRD...")
   process.dump = cms.EDAnalyzer(
     "DumpFEDRawDataProduct",
-    label=cms.untracked.InputTag('source'),
+    #label=cms.untracked.InputTag('source'),
+    label=cms.untracked.InputTag('HGCalSlinkFromRawSource', 'hgcalFEDRawData'),
     feds=cms.untracked.vint32(*options.fedId),
     dumpPayload=cms.untracked.bool(True)
   )
