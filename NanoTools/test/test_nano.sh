479e4ea27e545874c5879736119501c81a96dd2a
Pedro Vieira De Castro Ferreira Da Silva <pedro.silva@cern.ch>
2024-06-14 13:54:42 +0200

    Start a NANO area for the DPG with a RunInfo producer summarizing electronics

---
+++ NanoTools/test/test_nano.sh
@@ -0,0 +1,14 @@
+cmsDriver.py NANO \
+             -s USER:HGCalCommissioning/NanoTools/hgcSysValNano_cff.hgcSysValNanoTask \
+             --customise HGCalCommissioning/NanoTools/hgcSysValNano_cff.customise_elemapper \
+             --datatier NANOAOD \
+             --eventcontent NANOAOD \
+             --filein file:/afs/cern.ch/user/p/psilva/public/TB2024/output.root \
+             --fileout nano.root \
+             -n -1 \
+             --nThreads 4 \
+             --conditions auto:phase2_realistic_T25 \
+             --geometry Extended2026D99 \
+             --era Phase2C17I13M9 \
+             --python_filename nanocmsdriver_cfg.py \
+             --no_exec
