df8d5e8dd2b5d3af9cf23464f1123d96d0c56695
Yulun Miao <yulun.miao@cern.ch>
2024-07-13 00:37:46 +0200

    New nano format

--- NanoTools/python/hgcSysValNano_cff.py
+++ NanoTools/python/hgcSysValNano_cff.py
@@ -1,30 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 from PhysicsTools.NanoAOD.common_cff import Var
 
-# FIXME: these customises should be elsewhere
-
-
-def customise_elemapper(process):
-
-    modules = 'HGCalCommissioning/SystemTestEventFilters/data/ModuleMaps/modulelocator_test_2mods.txt'
-    sicells = 'Geometry/HGCalMapping/data/CellMaps/WaferCellMapTraces.txt'
-    sipmcells = 'Geometry/HGCalMapping/data/CellMaps/channels_sipmontile.hgcal.txt'
-
-    process.load('Geometry.HGCalMapping.hgCalMappingESProducer_cfi')
-    process.hgCalMappingESProducer.modules = cms.FileInPath(modules)
-    process.hgCalMappingESProducer.si = cms.FileInPath(sicells)
-    process.hgCalMappingESProducer.sipm = cms.FileInPath(sipmcells)
-    process.load('Configuration.StandardSequences.Accelerators_cff')
-    process.hgCalMappingCellESProducer = cms.ESProducer('hgcal::HGCalMappingCellESProducer@alpaka',
-                                                        filelist=cms.vstring(sicells, sipmcells),
-                                                        cellindexer=cms.ESInputTag(''))
-    process.hgCalMappingModuleESProducer = cms.ESProducer('hgcal::HGCalMappingModuleESProducer@alpaka',
-                                                          filename=cms.FileInPath(modules),
-                                                          moduleindexer=cms.ESInputTag(''))
-    return process
-
+#this should still be moved to a more central place HGCalCommissioning/Configuration ?
+from Geometry.HGCalMapping.hgcalmapping_cff import customise_hgcalmapper
+def customise_hgcalmapper_test(process):
+    return customise_hgcalmapper(process,
+                                 modules='HGCalCommissioning/SystemTestEventFilters/data/ModuleMaps/modulelocator_test_2mods.txt')
 
 # build NANO task
 hgcRunFEDReadoutTable = cms.EDProducer("HGCalRunFEDReadoutSequence")
-hgCalTB2023Table = cms.EDProducer("HGCalTB2023TableProducer")
-hgcSysValNanoTask = cms.Task(hgcRunFEDReadoutTable, hgCalTB2023Table)
+hgCalNanoTable = cms.EDProducer("HGCalNanoTableProducer")
+hgcSysValNanoTask = cms.Task(hgcRunFEDReadoutTable, hgCalNanoTable)
