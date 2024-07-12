f997f897e543a9fc5d40a57c20c13f754ebd640d
Pedro Vieira De Castro Ferreira Da Silva <pedro.silva@cern.ch>
2024-07-13 00:44:59 +0200

    Update in the Run table

--- NanoTools/plugins/HGCalRunFEDReadoutSequence.cc
+++ NanoTools/plugins/HGCalRunFEDReadoutSequence.cc
@@ -18,6 +18,7 @@
 
 // system include files
 #include <memory>
+#include <algorithm>
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -31,6 +32,7 @@
 #include "PhysicsTools/NanoAOD/interface/SimpleFlatTableProducer.h"
 #include "CondFormats/DataRecord/interface/HGCalElectronicsMappingRcd.h"
 #include "CondFormats/HGCalObjects/interface/HGCalMappingParameterHostCollection.h"
+#include "CondFormats/HGCalObjects/interface/HGCalMappingModuleIndexer.h"
 
 //
 // class declaration
@@ -40,8 +42,14 @@ class HGCalRunFEDReadoutSequence : public edm::one::EDProducer<edm::EndRunProduc
 public:
   //
   HGCalRunFEDReadoutSequence(edm::ParameterSet const &iConfig)
-      : tableName_(iConfig.getParameter<std::string>("tableName")), moduleTkn_(esConsumes<edm::Transition::EndRun>()) {
-    produces<nanoaod::FlatTable, edm::Transition::EndRun>(tableName_);
+      : tableName_(iConfig.getParameter<std::string>("tableName")),
+        typeCodeTableName_(iConfig.getParameter<std::string>("typeCodeTableName")), 
+        moduleTkn_(esConsumes<edm::Transition::EndRun>()),
+        moduleIndexTkn_(esConsumes<edm::Transition::EndRun>()) {
+    
+    produces<nanoaod::FlatTable,edm::Transition::EndRun>(tableName_);
+    produces<nanoaod::FlatTable,edm::Transition::EndRun>(typeCodeTableName_);
+    
   }
 
   //
@@ -50,7 +58,8 @@ public:
   //
   static void fillDescriptions(edm::ConfigurationDescriptions &descriptions) {
     edm::ParameterSetDescription desc;
-    desc.add<std::string>("tableName", "HGCFEDReadout");
+    desc.add<std::string>("tableName",         "HGCReadout");
+    desc.add<std::string>("typeCodeTableName", "HGCTypeCodes");
     descriptions.addWithDefaultLabel(desc);
   }
 
@@ -61,32 +70,59 @@ public:
   void produce(edm::Event &iEvent, const edm::EventSetup &iSetup) override {}
 
   void endRunProduce(edm::Run &iRun, const edm::EventSetup &iSetup) final {
-    //convert module mapping to rows of a flat table
+
+    //get module info and module indexer
     auto const &modules = iSetup.getData(moduleTkn_);
-    const size_t nmodules = modules.view().metadata().size();
-    std::vector<uint32_t> fed(nmodules), seq(nmodules), modtype(nmodules), celltype(nmodules);
-    for (size_t i = 0; i < nmodules; i++) {
-      auto imod = modules.view()[i];
-      fed[i] = imod.fedid();
-      seq[i] = imod.econdidx();
-      modtype[i] = imod.typeidx();
-      celltype[i] = imod.celltype();
+    auto const &moduleIndex = iSetup.getData(moduleIndexTkn_);
+
+    //built the table
+    //track the typecodes corresponding to each index
+    uint32_t nmodules = moduleIndex.getMaxModuleSize();
+    std::vector<uint32_t> fed(nmodules), seq(nmodules),plane(nmodules), i1(nmodules), i2(nmodules);    
+    std::vector<bool> isSiPM(nmodules), zside(nmodules);
+    typedef std::pair<std::string, std::vector<uint32_t> > TypeCode2Idx_t;
+    std::vector<TypeCode2Idx_t> typeCodeTreeIndices;
+    uint32_t idx(0);
+    for(auto it : moduleIndex.typecodeMap_) {
+
+      std::string typecode = it.first;
+      std::replace( typecode.begin(), typecode.end(), '-', '_');
+      typeCodeTreeIndices.push_back( TypeCode2Idx_t(typecode, {idx} ) );
+
+      fed[idx] = it.second.first;
+      seq[idx] = it.second.second;
+      uint32_t modIdx = moduleIndex.getTypeForModule(fed[idx], seq[idx]);
+      auto imod = modules.view()[modIdx];
+      isSiPM[idx] = imod.isSiPM();
+      zside[idx] = imod.zside();
+      plane[idx] = imod.plane();
+      i1[idx] = imod.i1();
+      i2[idx] = imod.i2();
+      idx++;
     }
+    
+    //the readout table
+    auto out_readout = std::make_unique<nanoaod::FlatTable>(nmodules, tableName_, false);
+    out_readout->addColumn<uint32_t>("FED", fed, "FED ID index");
+    out_readout->addColumn<uint32_t>("Seq", seq, "Module sequence in readout");
+    out_readout->addColumn<bool>("IsSiPM", isSiPM, "Is SiPM-on-tile?");
+    out_readout->addColumn<bool>("PositiveEndcap", zside, "Positive endcap?");
+    out_readout->addColumn<uint32_t>("Layer", plane, "Layer number");
+    out_readout->addColumn<uint32_t>("i1", i1, "Location index 1 (u or i-ring)");
+    out_readout->addColumn<uint32_t>("i2", i2, "Location index 2 (v or i-phi)");
+    iRun.put(std::move(out_readout), tableName_);
 
-    //the table
-    auto out = std::make_unique<nanoaod::FlatTable>(nmodules, tableName_, false);
-    out->addColumn<uint32_t>("FED", fed, "FED ID index");
-    out->addColumn<uint32_t>("Seq", seq, "Module sequence in readout");
-    out->addColumn<uint32_t>("ModuleType", modtype, "Module type");
-    out->addColumn<uint32_t>("CellType", celltype, "Cell type");
-
-    //put in the run
-    iRun.put(std::move(out), tableName_);
+    //the type codes for each entry in the table above
+    auto out_typecodes = std::make_unique<nanoaod::FlatTable>(1, typeCodeTableName_, false);
+    for(auto it : typeCodeTreeIndices)
+      out_typecodes->addColumn<uint32_t>(it.first, it.second, "Typecode index");
+    iRun.put(std::move(out_typecodes),typeCodeTableName_);
   }
 
 protected:
-  std::string tableName_;
+  std::string tableName_,typeCodeTableName_;
   edm::ESGetToken<hgcal::HGCalMappingModuleParamHostCollection, HGCalElectronicsMappingRcd> moduleTkn_;
+  edm::ESGetToken<HGCalMappingModuleIndexer, HGCalElectronicsMappingRcd> moduleIndexTkn_;
 };
 
 //define this as a plug-in
